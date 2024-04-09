54caf7f69419c81d4b8b532b2ebd804551749727
Alejandro <ale.torrubiano@gmail.com>
2024-04-09 12:33:14 +0200

    feat: añadido scss y configurado para que sea global y añadido componente lateral para mostrar la información principal

---
+++ src/components/AsideData.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="aside-container">
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .aside-container {
+        width: 30%;
+        height: 100vh;
+        background-color: map-get($map: $colors, $key: c-section-background);
+    }
+</style>
--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,15 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "@/assets/styles/_mixins.scss";
+          @import "@/assets/styles/_variables.scss";
+        `,
+      },
+    },
+  },
 })
